bcca630176c09d709a51e238e50f4a4aed879749
Vitalii Fedusov <vitalii.fedusov@gmail.com>
2024-09-02 22:31:29 +0300

    remove comment from Start.tsx

--- src/Components/Stars/Stars.tsx
+++ src/Components/Stars/Stars.tsx
@@ -2,19 +2,6 @@ import Box from '@mui/material/Box';
 import Rating from '@mui/material/Rating';
 import StarIcon from '@mui/icons-material/Star';
 
-// const labels: { [index: string]: string } = {
-//   0.5: 'Useless',
-//   1: 'Useless+',
-//   1.5: 'Poor',
-//   2: 'Poor+',
-//   2.5: 'Ok',
-//   3: 'Ok+',
-//   3.5: 'Good',
-//   4: 'Good+',
-//   4.5: 'Excellent',
-//   5: 'Excellent+',
-// };
-
 export default function TextRating() {
   const value = 4;
 
